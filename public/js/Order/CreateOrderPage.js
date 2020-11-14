6b8e635808a1543f4ea7ccab9c9bf196740244c8
Fleey <fleey2013@live.cn>
2020-11-14 17:46:44 +0800

    update.save code

--- public/js/Order/CreateOrderPage.js
+++ public/js/Order/CreateOrderPage.js
@@ -6,8 +6,21 @@ new Vue({
         }
     },
     methods: {
+        cancelSave() {
+            history.go(-1);
+        },
+        cancelQuickCreate() {
+            this.isQuickCreate = false;
+
+            $('input[name="total_num"]').val('');
+            $('input[name="product_price"]').val('');
+            $('input[name="discount_price"]').val('');
+            $('input[name="product_desc"]').val('');
+        },
         quickCreateEvent() {
             this.isQuickCreate = true;
+
+            let _this = this;
 
             setTimeout(function () {
                 $('select[name="productid"]').select2({
@@ -32,14 +45,101 @@ new Vue({
                             };
                         }
                     }
+                }).on('select2:select', function (e) {
+                    let data = e.params['data'];
+                    let price = data['price'] / 100;
+
+                    $('input[name="product_price"]').val(price);
+
+                    _this.totalNumChangeEvent();
                 });
             }, 200);
+        },
+        totalNumChangeEvent() {
+            let totalNum = parseInt($('input[name="total_num"]').val());
+
+            if (isNaN(totalNum) || totalNum <= 0)
+                return;
+
+            quickCreateChangeEvent();
+        },
+        productPriceChangeEvent() {
+            let productPrice = parseFloat($('input[name="product_price"]').val())
+
+            if (isNaN(productPrice) || productPrice <= 0)
+                return;
+
+            quickCreateChangeEvent();
+        },
+        saveQuickCreate() {
+            let productCount = parseInt($('input[name="total_num"]').val());
+            let productPrice = parseFloat($('input[name="product_price"]').val());
+
+            let discountPrice = parseFloat($('input[name="discount_price"]').val());
+            let productDesc = $('input[name="product_desc"]').val();
+
+            if (isNaN(productCount) || productCount <= 0) {
+                alert('商品数量不能为零或其他特殊字符');
+                return;
+            }
+
+            if (isNaN(productPrice) || productPrice <= 0) {
+                alert('商品价格不能为零或其他特殊字符');
+                return;
+            }
+
+            if (isNaN(discountPrice) || discountPrice <= 0) {
+                alert('商品总金额不能为零或其他特殊字符');
+                return;
+            }
+
+
+            let productInfo = $('select[name="productid"] option:selected');
+
+            addOrderInfo(productInfo.text(), productInfo.val(), productPrice, productCount, discountPrice, productDesc);
+
+            this.cancelQuickCreate();
         }
     }
 });
 
+function deleteOrderInfoRowEvent() {
+    $(this).parent().parent().remove();
+}
 
-$('select[name="productid"]').on('select2:select', function (e) {
-    var data = e.params.data;
-    console.log(data);
-});
+function addOrderInfo(productName, productId, productPrice, productCount, discountPrice, productDesc) {
+    let tableBodyDom = $('div.createOrderPage table>tbody');
+
+    let productNameDom = $(document.createElement('td')).text(productName).attr('data-product-id', productId);
+    let productPriceDom = $(document.createElement('td')).text(productPrice);
+    let productCountDom = $(document.createElement('td')).text(productCount);
+    let discountPriceDom = $(document.createElement('td')).text(discountPrice);
+    let productDescDom = $(document.createElement('td')).text(productDesc);
+
+    let trDom = $(document.createElement('tr'));
+
+    let editButtonDom = $(document.createElement('a')).attr({
+        'href': 'javascript:void(0);'
+    }).text('编辑');
+
+    let deleteButtonDom = $(document.createElement('a')).attr({
+        'href': 'javascript:void(0);'
+    }).text('删除').bind('click', deleteOrderInfoRowEvent);
+
+    let customActionDom = $(document.createElement('td')).append(editButtonDom, deleteButtonDom);
+
+    trDom.append(productNameDom, productCountDom, productPriceDom, discountPriceDom, productDescDom, customActionDom);
+
+    tableBodyDom.append(trDom);
+}
+
+function quickCreateChangeEvent() {
+    let productCount = $('input[name="total_num"]').val();
+    let productPrice = $('input[name="product_price"]').val();
+
+    let totalMoney = parseInt(productCount) * parseFloat(productPrice);
+
+    totalMoney = Number(totalMoney.toString().match(/^\d+(?:\.\d{0,2})?/));
+
+    $('input[name="discount_price"]').val(totalMoney);
+}
--- resources/views/Order/CreateOrderPage.php
+++ resources/views/Order/CreateOrderPage.php
@@ -26,47 +26,49 @@
 
                         <div class="form-inline create-form" v-if="isQuickCreate">
 
-                            &nbsp;<div class="input-group input-group-sm">
+                            &nbsp;
+                            <!--商品选择框-->
+                            <div class="input-group input-group-sm">
                                 <select class="form-control quick-create" name="productid"
                                         style="width: 150px;"></select>
                             </div>
-
+                            <!--商品选择框-->
                             &nbsp;
+                            <!--商品数量-->
                             <div class="input-group input-group-sm">
                                 <input style="width: 120px; text-align: right;" type="text" id="total_num"
+                                       @input="totalNumChangeEvent"
                                        name="total_num" value="" class="form-control total_num quick-create"
                                        placeholder="输入 下单数量"></div>
-                            &nbsp;<div class="input-group input-group-sm">
+                            <!--商品数量-->
+
+                            <!--商品单价-->
+                            <div class="input-group input-group-sm">
+                                <input style="width: 120px; text-align: right;" type="text" id="product_price"
+                                       @input="productPriceChangeEvent"
+                                       name="product_price" value="" class="form-control product_price quick-create"
+                                       placeholder="输入 商品单价"></div>
+                            <!--商品单价-->
+
+                            &nbsp;
+                            <!--商品总金额-->
+                            <div class="input-group input-group-sm">
                                 <input style="width: 200px" type="text" id="discount_price" name="discount_price"
                                        value="" class="form-control discount_price quick-create"
                                        placeholder="请输入 总计金额 不填则自动计算"></div>
+                            <!--商品总金额-->
                             &nbsp;
-                            <button class="btn btn-primary btn-sm">提交</button>&nbsp;
-                            <a href="javascript:void(0);" class="cancel" @click="isQuickCreate = false">取消</a>
+                            <!--商品备注-->
+                            <div class="input-group input-group-sm">
+                                <input style="width: 200px" type="text" id="product_desc" name="product_desc"
+                                       value="" class="form-control product_desc quick-create"
+                                       placeholder="请输入 备注信息 可不填"></div>
+                            <!--商品备注-->
+
+                            <button class="btn btn-primary btn-sm" type="button" @click="saveQuickCreate">提交</button>&nbsp;
+                            <a href="javascript:void(0);" class="cancel" @click="cancelQuickCreate">取消</a>
                             <input type="hidden" name="_token" value="XjGTC1A1V1qzwHsfY1jHLwVBjEuTfBn3HxqX8uxu">
                         </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td>183</td>
-                    <td>John Doe</td>
-                    <td>11-7-2014</td>
-                    <td><span class="label label-success">Approved</span></td>
-                    <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                    <td>
-                        <button class="btn btn-primary btn-sm" type="button">编辑</button>
-                        <button class="btn btn-danger btn-sm" type="button">删除</button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>219</td>
-                    <td>Alexander Pierce</td>
-                    <td>11-7-2014</td>
-                    <td><span class="label label-warning">Pending</span></td>
-                    <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                    <td>
-                        <button class="btn btn-primary btn-sm" type="button">编辑</button>
-                        <button class="btn btn-danger btn-sm" type="button">删除</button>
                     </td>
                 </tr>
                 </tbody>
@@ -74,9 +76,49 @@
         </div>
         <!-- /.box-body -->
         <div class="box-footer">
-            <button type="submit" class="btn btn-default">取消</button>
+            <button type="button" class="btn btn-default" @click="cancelSave">取消</button>
             <button type="submit" class="btn btn-info pull-right">保存</button>
         </div>
         <!-- /.box-footer -->
     </form>
+</div>
+<div class="modal fade in" id="editOrderInfoModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span></button>
+                <h4 class="modal-title">编辑订单商品信息</h4>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <label>商品类型</label>
+                    <select name="" id=""></select>
+                    <input type="text" class="form-control" placeholder="Enter ...">
+                </div>
+                <div class="form-group">
+                    <label>商品数量</label>
+                    <input type="text" class="form-control" placeholder="请输入商品数量">
+                </div>
+                <div class="form-group">
+                    <label>商品单价</label>
+                    <input type="text" class="form-control" placeholder="请输入商品单价">
+                </div>
+                <div class="form-group">
+                    <label>商品总金额</label>
+                    <input type="text" class="form-control" placeholder="请输入商品总金额">
+                </div>
+                <div class="form-group">
+                    <label>备注信息</label>
+                    <input type="text" class="form-control" placeholder="请输入备注信息">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">取消</button>
+                <button type="button" class="btn btn-primary">保存修改</button>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
 </div>
